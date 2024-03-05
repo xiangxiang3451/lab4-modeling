d9c79b87d9b6665a1c49c6bcd9e7156a37c31de6
Chang <xiangxiang3451@gmail.com>
2024-03-06 01:43:04 +0700

    first commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.PauseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 20;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(407, 80);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 20;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(381, 381);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(105, 135);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(118, 82);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click_1);
+            // 
+            // PauseButton
+            // 
+            this.PauseButton.Location = new System.Drawing.Point(105, 329);
+            this.PauseButton.Name = "PauseButton";
+            this.PauseButton.Size = new System.Drawing.Size(118, 82);
+            this.PauseButton.TabIndex = 3;
+            this.PauseButton.Text = "Stop";
+            this.PauseButton.UseVisualStyleBackColor = true;
+            this.PauseButton.Click += new System.EventHandler(this.PauseButton_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 578);
+            this.Controls.Add(this.PauseButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button PauseButton;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private bool[,] grid;
+        private bool[,] nextGrid;
+        private bool isRunning;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGrid();
+
+            timer = new Timer();
+            timer.Interval = 500;  // Set the refresh interval to 0.5 seconds
+            timer.Tick += Timer1_Tick;
+
+            grid = new bool[20, 20]; // Initialize the grid
+            nextGrid = new bool[20, 20]; // Initialize the grid of the next state
+        }
+
+        private void InitializeGrid()
+        {
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.BackColor = Color.Black;
+
+            //add rows and columns
+            for (int i = 0; i < 20; i++)
+            {
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50f));
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50f));
+            }
+
+            // Add grid cells and register click events
+            for (int row = 0; row < 20; row++)
+            {
+                for (int col = 0; col < 20; col++)
+                {
+                    Panel panel = new Panel();
+                    panel.Dock = DockStyle.Fill;
+                    panel.Margin = new Padding(0);
+                    panel.BackColor = Color.White;
+                    panel.Click += Panel_Click; // add click event
+
+                    tableLayoutPanel1.Controls.Add(panel, col, row);
+                }
+            }
+        }
+        private void Panel_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                Panel panel = sender as Panel;
+                if (panel != null)
+                {
+                    int row = tableLayoutPanel1.GetPositionFromControl(panel).Row;
+                    int col = tableLayoutPanel1.GetPositionFromControl(panel).Column;
+                    grid[row, col] = !grid[row, col];
+                    panel.BackColor = grid[row, col] ? Color.Black : Color.White;
+                }
+            }
+        }
+        private void ApplyCustomRule()
+        {
+            //            rules:
+            //            If a cell is alive and there are 2 living cells around it,
+            //            or there is 1 living cell around it, it will remain alive at
+            //            the next moment.
+            //            Otherwise, the cell will die at the next moment.
+            for (int row = 0; row < 20; row++)
+            {
+                for (int col = 0; col < 20; col++)
+                {
+                    int aliveNeighbors = CountAliveNeighbors(row, col);
+
+                    if ((grid[row, col] && aliveNeighbors == 2) || aliveNeighbors == 1)
+                    {
+                        nextGrid[row, col] = true;
+                    }
+                    else
+                    {
+                        nextGrid[row, col] = false;
+                    }
+                }
+            }
+        }
+        private int CountAliveNeighbors(int row, int col)
+        {
+            int count = 0;
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                for (int j = col - 1; j <= col + 1; j++)
+                {
+                    if (i >= 0 && i < 20 && j >= 0 && j < 20 && !(i == row && j == col))
+                    {
+                        if (grid[i, j])
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+        private void UpdateGrid()
+        {
+            for (int row = 0; row < 20; row++)
+            {
+                for (int col = 0; col < 20; col++)
+                {
+                    grid[row, col] = nextGrid[row, col];
+                    UpdatePanelColor(row, col);
+                }
+            }
+        }
+
+        private void UpdatePanelColor(int row, int col)
+        {
+            Panel panel = tableLayoutPanel1.GetControlFromPosition(col, row) as Panel;
+            if (panel != null)
+            {
+                panel.BackColor = grid[row, col] ? Color.Black : Color.White;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            ApplyCustomRule(); //reply of rule
+            UpdateGrid();
+        }
+        private void PauseButton_Click_1(object sender, EventArgs e)
+        {
+            isRunning = false;
+            timer.Stop();
+        }
+
+        private void StartButton_Click_1(object sender, EventArgs e)
+        {
+            isRunning = true;
+            timer.Start();
+        }
+    }
+}
